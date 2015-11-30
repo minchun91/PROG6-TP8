98d6f5a93b7fa07c8b4c78301f691f77bdd4db4c
husni <zulhusni.rahman@gmail.com>
2015-11-30 15:09:48 +0100

    Detruire fap itérative.

--- fap_bug.c
+++ fap_bug.c
@@ -63,6 +63,11 @@ int est_fap_vide(fap f)
 void
 detruire_fap(fap f)
 {
-  if (f != NULL)
-      free(f);
+ fap tmp;
+  while (f != NULL)
+	{
+	      tmp = f;
+	      f = f->prochain;
+	      free(tmp);
+	}
 }
